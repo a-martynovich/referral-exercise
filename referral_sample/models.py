57b66cc42205b234bed715323b2c89e57ee7a641
Artem Martynovich <artem.martynovich@gmail.com>
2020-05-11 19:48:31 +0600

    Implement inviter reward.

--- referral_sample/models.py
+++ referral_sample/models.py
@@ -1,22 +1,47 @@
+from uuid import uuid4
+
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+def random_token():
+    return uuid4().hex
+
+
 class Referral(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    token = models.CharField(blank=True, null=True, max_length=512)
+    token = models.CharField(default=random_token, max_length=512)
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     referral = models.OneToOneField(Referral, null=True, on_delete=models.CASCADE)
+    people_invited = models.PositiveIntegerField(default=0)
     balance = models.PositiveIntegerField(default=0)
 
     def update_referral(self, ref_token):
-        self.balance = 10
-        self.save(update_fields=['balance'])
+        ref = Referral.objects.filter(token=ref_token)
+        fields = ['referral']
+        if ref.exists():
+            ref = ref.first()
+            self.balance = settings.INVITEE_REWARD
+            ref.profile.reward_inviter()
+            fields.append('balance')
+
+        self.referral = Referral.objects.create()
+        self.save(update_fields=fields)
+
+    def reward_inviter(self):
+        self.people_invited += 1
+        fields = ['people_invited']
+        if self.people_invited == settings.INVITER_TARGET_COUNT:
+            self.people_invited = 0
+            self.balance += settings.INVITER_REWARD
+            fields.append('balance')
+        self.save(update_fields=fields)
 
 
 @receiver(post_save, sender=User)
--- referral_sample/views.py
+++ referral_sample/views.py
@@ -1,15 +1,11 @@
-import os
-
-from django.conf import settings
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.utils.http import urlencode
 from django.views.generic import TemplateView
-
-from .models import Referral
 
 
 class RootView(LoginRequiredMixin, TemplateView):
@@ -17,11 +13,9 @@ class RootView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx['db_info'] = settings.DATABASES
-        ctx['env'] = os.environ
-        ctx['refs'] = Referral.objects.all()
-
-        ctx['referral_link'] = self.request.build_absolute_uri(reverse('signup'))
+        ref_url = self.request.build_absolute_uri(reverse('signup')) + '?' + urlencode(
+            {'ref': self.request.user.profile.referral.token})
+        ctx['referral_link'] = ref_url
         ctx['balance'] = self.request.user.profile.balance
         return ctx
 
@@ -39,7 +33,9 @@ class SignupView(TemplateView):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-            new_user.profile.update_referral(request.GET.get('ref'))
+            ref = request.GET.get('ref')
+            if ref:
+                new_user.profile.update_referral(ref)
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
