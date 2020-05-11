c7a65e8da76c7765704514178d655f6e3f61ead6
Artem Martynovich <artem.martynovich@gmail.com>
2020-05-11 17:34:55 +0600

    Create user profile. Display balance and ref link.

--- referral_sample/models.py
+++ referral_sample/models.py
@@ -1,6 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Referral(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     token = models.CharField(blank=True, null=True, max_length=512)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    referral = models.OneToOneField(Referral, null=True, on_delete=models.CASCADE)
+    balance = models.PositiveIntegerField(default=0)
+
+    def update_referral(self, ref_token):
+        self.balance = 10
+        self.save(update_fields=['balance'])
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
--- referral_sample/views.py
+++ referral_sample/views.py
@@ -1,10 +1,12 @@
 import os
 
 from django.conf import settings
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.views.generic import TemplateView
 
 from .models import Referral
@@ -17,8 +19,10 @@ class RootView(LoginRequiredMixin, TemplateView):
         ctx = super().get_context_data(**kwargs)
         ctx['db_info'] = settings.DATABASES
         ctx['env'] = os.environ
-
         ctx['refs'] = Referral.objects.all()
+
+        ctx['referral_link'] = self.request.build_absolute_uri(reverse('signup'))
+        ctx['balance'] = self.request.user.profile.balance
         return ctx
 
 
@@ -34,8 +38,13 @@ class SignupView(TemplateView):
     def post(self, request, *args, **kwargs):
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')
+            new_user = form.save()
+            new_user.profile.update_referral(request.GET.get('ref'))
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('root')
         else:
             return self.render_to_response(self.get_context_data(form=form))
 
